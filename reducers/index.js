a04c1e9eb407e4fffbdd30ce983b40b09fbeabc2
Martijn Coevert <m.coevert@wewantweb.nl>
2017-10-18 15:08:16 +0200

    type: Adding redux  - Create store/actions/reducers

---
+++ reducers/index.js
@@ -0,0 +1,10 @@
+// import {} from "../actions/index";
+
+function decks(state = {}, action) {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
+
+export default decks;
