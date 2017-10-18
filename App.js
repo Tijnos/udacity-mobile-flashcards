31e3269def22e2aea3b09c9bdc9b4ce7da9bee8d
Martijn Coevert <m.coevert@wewantweb.nl>
2017-10-18 15:37:28 +0200

    type: Added tab navigation

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Platform } from 'react-native';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
-import { Constants } from 'expo'
-import {orange} from "./utils/colors";
+import { Constants, Pl } from 'expo'
+import {TabNavigator} from 'react-navigation';
+import DeckList from './components/DeckList';
+import {white, orange} from "./utils/colors";
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+
+const Tabs = new TabNavigator({
+    DeckList: {
+        screen: DeckList,
+        navigationOptions: {
+          tabBarLabel: 'Deck list',
+          tabBarIcon: ({ tintColor }) => <MaterialCommunityIcons name='cards' size={30} color={tintColor} />
+        }
+    },
+}, {
+    tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? orange : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : orange,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+}});
 
 export default class App extends React.Component {
   render() {
@@ -14,9 +41,7 @@ export default class App extends React.Component {
                 <View style={{ backgroundColor: orange, height: Constants.statusBarHeight }}>
                     <StatusBar translucent backgroundColor={orange} barStyle="light-content" />
                 </View>
-                <View style={styles.container}>
-                    <Text>App</Text>
-                </View>
+                <Tabs/>
             </View>
         </Provider>
     );
